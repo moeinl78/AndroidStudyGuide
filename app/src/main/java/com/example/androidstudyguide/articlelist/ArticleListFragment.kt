2e81e4bc099247223077ec82eedb7b663dbdb0e3
MoeinLatifi <moeinlatifi7117@gmail.com>
2022-10-21 20:13:42 +0330

    Add recycler view setup for the 'articlelist' feature #5.

--- app/src/main/java/com/example/androidstudyguide/articlelist/ArticleListFragment.kt
+++ app/src/main/java/com/example/androidstudyguide/articlelist/ArticleListFragment.kt
@@ -5,22 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.androidstudyguide.databinding.FragmentArticleListBinding
 
 class ArticleListFragment : Fragment() {
 
     private lateinit var binding: FragmentArticleListBinding
+    private lateinit var mAdapter: ArticleListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        mAdapter = ArticleListAdapter()
         binding = FragmentArticleListBinding.inflate(layoutInflater, container, false)
+        setupRecyclerView()
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private fun setupRecyclerView() {
+        val itemDivider = DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
+        binding.fragmentArticleListRecycler.apply {
+            adapter = mAdapter
+            layoutManager = LinearLayoutManager(context)
+            addItemDecoration(itemDivider)
+        }
     }
 }
