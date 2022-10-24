3fbef7a44ad9cfa2bdadca913f6038c482cf2e25
Moein Latifi <74295956+moeinl78@users.noreply.github.com>
2022-10-24 15:33:45 +0330

    Add extension function for view visibility #18. (#19)

--- app/src/main/java/com/example/androidstudyguide/utils/extensions/Extensions.kt
+++ app/src/main/java/com/example/androidstudyguide/utils/extensions/Extensions.kt
@@ -1,5 +1,6 @@
 package com.example.androidstudyguide.utils.extensions
 
+import android.view.View
 import com.example.androidstudyguide.data.remote.essence.dto.EssenceFeedItemDto
 import com.example.androidstudyguide.models.Article
 import com.example.androidstudyguide.utils.EncodeString
@@ -10,4 +11,12 @@ fun EssenceFeedItemDto.toArticle(): Article {
         authorName = this.author.name,
         url = this.link.href
     )
+}
+
+fun View.visibleIf(condition: Boolean) {
+    this.visibility = if (condition) {
+        View.VISIBLE
+    } else {
+        View.GONE
+    }
 }
