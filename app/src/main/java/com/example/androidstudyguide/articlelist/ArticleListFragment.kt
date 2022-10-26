7273d5fcf01ccbc1272618fd7641e66cd1f0c6a9
MoeinLatifi <moeinlatifi7117@gmail.com>
2022-10-26 13:02:30 +0330

    Reformat the code with ktlint #16.

--- app/src/main/java/com/example/androidstudyguide/articlelist/ArticleListFragment.kt
+++ app/src/main/java/com/example/androidstudyguide/articlelist/ArticleListFragment.kt
@@ -8,12 +8,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.androidstudyguide.R
-import com.example.androidstudyguide.data.repository.ArticleRepository
 import com.example.androidstudyguide.databinding.FragmentArticleListBinding
 import com.example.androidstudyguide.dependencyGraph
 import com.example.androidstudyguide.models.Article
